5a253fc6feeaafc6617a87ff76fa214d688b0a04
IPMC <IPMC@134.59.171.132>
2019-10-17 16:43:06 +0100

    v1.0

--- subsample_flow.sh
+++ subsample_flow.sh
@@ -1,3 +1,7 @@
+#!/bin/bash
+
+# need Java.1.8 in JAVA_HOME
+java=`which $JAVA_HOME`
 
 # clone repository should have been done already
 # git clone https://github.com/ucagenomix/sicelore.git
@@ -8,10 +12,10 @@
 mkdir output_dir
 
 # parse illumina bam file
-java -jar Jar/IlluminaParser-1.0.jar -i Data/190c.clta.illumina.bam -o output_dir/190c.clta.illumina.bam.obj -t Barcodes/cellBC.190.tsv -b CB -g GN -u UB
+$java -jar Jar/IlluminaParser-1.0.jar -i Data/190c.clta.illumina.bam -o output_dir/190c.clta.illumina.bam.obj -t Barcodes/cellBC.190.tsv -b CB -g GN -u UB
 
 # scan nanopore reads
-java -jar Jar/NanoporeReadScanner-0.5.jar -i Data/190c.clta.nanopore.reads.fq -o output_dir
+$java -jar Jar/NanoporeReadScanner-0.5.jar -i Data/190c.clta.nanopore.reads.fq -o output_dir
 
 # map reads to genome
 minimap2 -ax splice -uf --MD --sam-hit-only -t 4 --junc-bed Gencode/gencode.v18.mm10.junctions.bed Data/chr4.fa.gz output_dir/passed/190c.clta.nanopore.readsFWD.fq > output_dir/minimap.sam
@@ -20,20 +24,36 @@ samtools sort output_dir/minimap.unsorted.bam -o output_dir/minimap.bam
 samtools index output_dir/minimap.bam
 
 # tag reads with gene name
-java -jar -Xmx12g Jar/Sicelore-1.0.jar AddGeneNameTag I=output_dir/minimap.bam O=output_dir/GE.bam REFFLAT=Gencode/gencode.v18.mm10.refFlat.txt GENETAG=GE ALLOW_MULTI_GENE_READS=true USE_STRAND_INFO=true VALIDATION_STRINGENCY=SILENT
+$java -jar -Xmx4g Jar/Sicelore-1.0.jar AddGeneNameTag I=output_dir/minimap.bam O=output_dir/GE.bam REFFLAT=Gencode/gencode.v18.mm10.refFlat.txt GENETAG=GE ALLOW_MULTI_GENE_READS=true USE_STRAND_INFO=true VALIDATION_STRINGENCY=SILENT
 samtools index output_dir/GE.bam
 
 # tag reads with fastq sequence
-java -jar -Xmx12g Jar/Sicelore-1.0.jar AddBamReadSequenceTag I=output_dir/GE.bam O=output_dir/GEUS.bam FASTQ=output_dir/passed/190c.clta.nanopore.readsFWD.fq VALIDATION_STRINGENCY=SILENT
+$java -jar -Xmx4g Jar/Sicelore-1.0.jar AddBamReadSequenceTag I=output_dir/GE.bam O=output_dir/GEUS.bam FASTQ=output_dir/passed/190c.clta.nanopore.readsFWD.fq VALIDATION_STRINGENCY=SILENT
 samtools index output_dir/GEUS.bam
 
 # tag reads with cellBC/UMI barcodes
-java -jar -Xmx12g Jar/NanoporeBC_UMI_finder-1.0.jar -i output_dir/GEUS.bam -o output_dir/GEUS10xAttributes.bam -k output_dir/190c.clta.illumina.bam.obj --maxUMIfalseMatchPercent 1 --maxBCfalseMatchPercent 5 --logFile output_dir/out.log
+$java -jar -Xmx4g Jar/NanoporeBC_UMI_finder-1.0.jar -i output_dir/GEUS.bam -o output_dir/GEUS10xAttributes.bam -k output_dir/190c.clta.illumina.bam.obj --maxUMIfalseMatchPercent 1 --maxBCfalseMatchPercent 5 --logFile output_dir/out.log
 
 # generate isoform matrix
-java -jar -Xmx12g Jar/Sicelore-1.0.jar IsoformMatrix DELTA=2 METHOD=STRICT GENETAG=GE I=output_dir/GEUS10xAttributes_umifound_.bam REFFLAT=Gencode/gencode.v18.mm10.refFlat.txt CSV=Barcodes/cellBC.190.tsv OUTDIR=output_dir PREFIX=sicelore VALIDATION_STRINGENCY=SILENT
+$java -jar -Xmx4g Jar/Sicelore-1.0.jar IsoformMatrix DELTA=2 METHOD=STRICT GENETAG=GE I=output_dir/GEUS10xAttributes_umifound_.bam REFFLAT=Gencode/gencode.v18.mm10.refFlat.txt CSV=Barcodes/cellBC.190.tsv OUTDIR=output_dir PREFIX=sicelore.reads VALIDATION_STRINGENCY=SILENT
 
 # compute consensus sequence
-java -jar -Xmx12g Jar/Sicelore-1.0.jar ComputeConsensus T=10 I=output_dir/GEUS10xAttributes_umifound_.bam O=output_dir/consensus.fa
+$java -jar -Xmx4g Jar/Sicelore-1.0.jar ComputeConsensus T=10 I=output_dir/GEUS10xAttributes_umifound_.bam O=output_dir/consensus.fa
 
-# ...
+# map molecules to genome
+minimap2 -ax splice -uf --MD --sam-hit-only -t 4 --junc-bed Gencode/gencode.v18.mm10.junctions.bed Data/chr4.fa.gz output_dir/consensus.fa > output_dir/molecule.sam
+samtools view -Sb output_dir/molecule.sam -o output_dir/molecule.unsorted.bam
+samtools sort output_dir/molecule.unsorted.bam -o output_dir/molecule.bam
+samtools index output_dir/molecule.bam
+
+# add cellBCs/UMIs tags to molecules SAM records
+$java -jar -Xmx4g Jar/Sicelore-1.0.jar AddBamMoleculeTags I=output_dir/molecule.bam O=output_dir/molecule.tags.bam
+samtools index output_dir/molecule.tags.bam
+
+# tag molecules with gene name and export percentage identity
+$java -jar -Xmx22g Jar/Sicelore-1.0.jar AddGeneNameTag I=output_dir/molecule.tags.bam O=output_dir/molecule.tags.GE.bam REFFLAT=Gencode/gencode.v18.mm10.refFlat.txt GENETAG=GE ALLOW_MULTI_GENE_READS=true USE_STRAND_INFO=true VALIDATION_STRINGENCY=SILENT
+samtools index output_dir/molecule.tags.GE.bam
+$java -jar -Xmx4g Jar/Sicelore-1.0.jar ExportMoleculeMetrics I=output_dir/molecule.tags.bam O=output_dir/sicelore_molecules_metrics.txt GENETAG=GE
+	
+# generate molecule isoform matrix
+$java -jar -Xmx4g Jar/Sicelore-1.0.jar IsoformMatrix DELTA=2 METHOD=STRICT I=output_dir/molecule.tags.GE.bam REFFLAT=Gencode/gencode.v18.mm10.refFlat.txt GENETAG=GE CSV=Barcodes/cellBC.190.tsv OUTDIR=output_dir PREFIX=sicelore.molecule VALIDATION_STRINGENCY=SILENT
